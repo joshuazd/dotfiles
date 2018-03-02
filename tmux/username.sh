e46be5bafce985a00f079f90c8c57f8fb7f34b6e
joshuazd <zinkdudajoshua@gmail.com>
2018-03-02 13:24:05 -0600

    Fix tmux hostname and username scripts to work with sshrc

--- tmux/username.sh
+++ tmux/username.sh
@@ -6,6 +6,7 @@ username() {
   #   [ -n "$pid" ] && ssh_parameters=$(tr '\0' ' ' < "/proc/$pid/cmdline" | sed 's/^ssh //')
   # else
   ssh_parameters=$(ps -t "$tty" -o command= | awk '/ssh/ && !/vagrant ssh/ && !/autossh/ && !/-W/ { $1=""; print $0; exit }')
+  ssh_parameters=$(echo "$ssh_parameters" | awk '{ if($1~/sshrc/) print $2; else print $0; }')
   if [ -n "$ssh_parameters" ]; then
     # shellcheck disable=SC2086
     username=$(ssh -G $ssh_parameters 2>/dev/null | awk 'NR > 2 { exit } ; /^user / { print $2 }')
