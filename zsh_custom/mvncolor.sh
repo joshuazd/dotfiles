8d19eb5231d9b6ba86482a6cf5721e95f5cb36b3
joshuazd <zinkdudajoshua@gmail.com>
2018-06-01 10:01:09 -0500

    Remove unused color variables

--- zsh_custom/mvncolor.sh
+++ zsh_custom/mvncolor.sh
@@ -10,14 +10,6 @@ export TEXT_BLUE=`tput setaf 12`
 export TEXT_MAGENTA=`tput setaf 5`
 export TEXT_CYAN=`tput setaf 6`
 export TEXT_WHITE=`tput setaf 7`
-export BACKGROUND_BLACK=`tput setab 0`
-export BACKGROUND_RED=`tput setab 1`
-export BACKGROUND_GREEN=`tput setab 2`
-export BACKGROUND_YELLOW=`tput setab 3`
-export BACKGROUND_BLUE=`tput setab 4`
-export BACKGROUND_MAGENTA=`tput setab 5`
-export BACKGROUND_CYAN=`tput setab 6`
-export BACKGROUND_WHITE=`tput setab 7`
 export RESET_FORMATTING=`tput sgr0`
  
 # Wrapper function for Maven's mvn command.
